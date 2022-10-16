70a4f5e6c4906dcff731664fafa5d054f99930b1
Lars Mueller <lars3.mueller(at)student.uni-siegen.de>
2022-10-16 16:45:05 +0200

    ÜbenCommit

---
+++ Variables/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Varaibles
+{
+    static void Main(string[] args)
+    {
+        string name = "Lars Müller";
+        string date = "12.02.1995";
+
+        Console.WriteLine("Hallo " + name + " geboren am " + date);
+
+    }
+}
